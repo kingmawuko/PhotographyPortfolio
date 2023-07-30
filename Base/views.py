1de4799217c79eb637d867336e30f8927e8db6bd
Mawuko <mawukoattivor@gmail.com>
2023-07-30 16:43:29 -0400

    checkpoint

--- Base/views.py
+++ Base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from django.contrib import messages
-from django.core.mail import send_mail
+
 from django.shortcuts import *
 
 # Create your views here.
@@ -18,13 +18,10 @@ def ContactView(request):
         Name = request.POST.get('name')    
         print({'Email':Email,'Name':Name,'message':message})
         contact=Contact.objects.create(Name=Name,Email=Email,Message=message)
-        #Message = f' "{contact.Subject}" form submitted from {contact.email}. They mentioned in their message ({contact.Message})'
+     
         print({'Email':Email,'message':message,'Name':Name})
         messages.info(request,'Thank you for your message. You will be contacted soon. Form submission successful') 
-        #Subject = f' ! Freelance Service !'
-        #email = 'mawukoattivor@gmail.com'
-        #send_mail(Subject, Message, email, [email], fail_silently=False)
-        # Message.messages
+        
 
     context = {}
     return render(request,'Base/Contact.html',context)
@@ -37,7 +34,7 @@ def PolicyView(request):
 def QuestionsView(request):
     Faq=CommonQuestion.objects.all()
 
-    # Common Questions 
+    
     context = {'Faq':Faq}
     return render(request,'Base/Questions.html',context)
 
@@ -96,24 +93,16 @@ def InvoiceReviewView(request,InvoiceNum):
 def InvoicePartialView(request, InvoiceNum):
     Invoice = get_object_or_404(InvoiceModel, Invoice=InvoiceNum)
     if Invoice: 
-        # if the invoice status is partial set the EmailStatus to Partial too and save else none 
+       
         if Invoice.status != 'partial':
             Invoice.EmailStatus = 'Partial'
             print({'Invoice status ':Invoice.status})
             print({' EmailStatus':Invoice.EmailStatus})
             Invoice.status = 'partial'
             Invoice.save()
-            # if the Emailstatus is partial send both emails 
+          
             if Invoice.EmailStatus == 'Partial':
-               # client_message = f'Thanks. Your payment has been processed and I will begin work on your project. If you have any questions, please feel free to ask.'
-                #client_subject = f' Payment Received'
-                #client_email =Invoice.Email
-                #send_mail(client_subject, client_message, client_email, [Invoice.Email], fail_silently=False)
-               
-                #admin_message = f'A partial payment has been received from {Invoice.Email}.'
-                #admin_subject = f'! Portfolio Message ! Partial Payment Received'
-                #admin_email = 'mawukoattivor@gmail.com' 
-                #send_mail(admin_subject, admin_message, client_email, [admin_email], fail_silently=False)
+             
                 Invoice.EmailStatus = 'Partial' 
             else:
                 None  
@@ -127,14 +116,7 @@ def InvoiceCompleteView(request, InvoiceNum):
             Invoice.status = 'completed'
             Invoice.save()       
             if Invoice.EmailStatus == 'None':    
-                client_message = f'Thanks. Your payment has been completed and your project is now finished. If you have any questions or concerns, please feel free to contact me for support, upgrades, or add-ons.'
-                client_subject = f'Payment Completed'
-               # client_email = Invoice.Email
-               # send_mail(client_subject, client_message, client_email, [Invoice.Email], fail_silently=False)          
-                #admin_message = f'{Invoice.Email} has completed the payment for their project.'
-                #admin_subject = f'! Portfolio Message ! Payment Completed'
-                #admin_email = 'mawukoattivor@gmail.com' 
-                #send_mail(admin_subject, admin_message, client_email, [admin_email], fail_silently=False)
+               
                 Invoice.EmailStatus == 'Partial'
             else:
                 None
